240b95a6490f02c31183334c742f57aac89a1ce2
tianxingkk <138958339+tianxingkk@users.noreply.github.com>
2023-07-08 17:58:18 +0800

    Update ts.php

--- api/ts.php
+++ api/ts.php
@@ -1,12 +1,13 @@
 <?php
 if(isset($_GET['id'])){
     $url = $_GET['id'];
+       $url = "http://123.51.149.52:5003/".$id."";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
- curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78' );
+	curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78' );
     //curl_setopt($ch, CURLOPT_REFERER, 'https://www.ofiii.com/');
     $content = curl_exec($ch);
     curl_close($ch);
